be5046383053c0595089db81609531fd75dc0048
Zakariya Shahid <zakariyashahid511@gmail.com>
2022-09-14 21:14:13 +0500

    question 2 updated

--- second.go
+++ second.go
@@ -4,43 +4,59 @@ import (
 	"fmt"
 )
 
-type car struct {
-	brand string
-	model string
-	reg   int
+type employee struct {
+	name     string
+	position string
+	salary   int
 }
 
-func display(vehicle car) {
-	fmt.Print("Brand of the car = ", vehicle.brand, "\n")
-	fmt.Print("Model of the car = ", vehicle.model, "\n")
-	fmt.Print("Registration number of the car = ", vehicle.reg, "\n")
+type company struct {
+	companyName string
+	employees   []employee
+}
+
+func displayEmployee(emp employee) {
+	fmt.Print("\nName = ", emp.name, "\n")
+	fmt.Print("Position = ", emp.position, "\n")
+	fmt.Print("Salary = ", emp.salary, "\n")
+}
+
+func displayCompany(comp company) {
+	fmt.Println("\nCompany name = ", comp.companyName)
+
+	for j := 0; j < len(comp.employees); j++ {
+
+		fmt.Println("\n\tEmployee ", j+1)
+		displayEmployee(comp.employees[j])
+		fmt.Println("\n")
+	}
+
 }
 
 func main() {
 	var num int
 	//var check string
-	fmt.Print("Enter the number of cars : ")
+	fmt.Print("Enter the number of employees you want to register : ")
 	fmt.Scanf("%d", &num)
 
-	vehicles := make([]car, num)
-	//taking inputs for the details of the struc car
-	for i := 0; i < num; i++ {
+	employeeList := make([]employee, num)
+	emp1 := employee{"Zakariya", "Developer", 340000}
+	employeeList[0] = emp1
+	//taking inputs for the details of the struc employee
+	for i := 1; i < num; i++ {
 
 		//fmt.Scan(&check)
-		fmt.Print("Enter the brand of the car ", i+1, " : ")
-		fmt.Scan(&vehicles[i].brand)
-		fmt.Print("Enter the model of the car ", i+1, " : ")
-		fmt.Scan(&vehicles[i].model)
-		fmt.Print("Enter the registration number of the car ", i+1, " : ")
-		fmt.Scan(&vehicles[i].reg)
-		fmt.Println("\n")
-	}
-	//passing the struct in fucntion
-	for j := 0; j < num; j++ {
-
-		fmt.Println("\n\tCar ", j+1)
-		display(vehicles[j])
+		fmt.Print("Enter the name of employee ", i+1, " : ")
+		fmt.Scan(&employeeList[i].name)
+		fmt.Print("Enter the position of the employee ", i+1, " : ")
+		fmt.Scan(&employeeList[i].position)
+		fmt.Print("Enter the salary of the employee ", i+1, " : ")
+		fmt.Scan(&employeeList[i].salary)
 		fmt.Println("\n")
 	}
 
+	//creating company struct
+	comp := company{"Tetra", employeeList}
+
+	displayCompany(comp)
 }
